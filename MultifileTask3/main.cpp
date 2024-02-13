a599429576004168377e60a6d9d114e9870bc6b1
flam1ngo <kris.s.03@mail.ru>
2024-02-13 12:15:44 +0300

    Initial Commit

---
+++ MultifileTask3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MultifileTask3
+//
+//  Created by Кристина Скворцова on 13.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
